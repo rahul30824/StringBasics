60382a4aafc9ed23024c8c8e5621b49d5228211e
rahul30824 <rahulr684.sse@saveetha.com>
2025-01-26 20:08:13 +0530

    Create Reverse_a_string.java

---
+++ Reverse_a_string.java
@@ -0,0 +1,16 @@
+class Main
+{
+   public static String ReverseAstring(String str)
+   {
+       for(int i=str.length()-1;i>=0;i--)
+       {
+           System.out.print(str.charAt(i));
+       }
+       return str;
+   }
+   public static void main(String[] args)
+   {
+     String str="lugar";
+     ReverseAstring(str);
+   }
+}
